e155be868ac793d8418cb5119121d57b3c0a1c53
nadjet <nadjetba@gmail.com>
2021-11-26 15:26:27 +0100

    Moved files around, renamed imported packages

--- pet/tasks.py
+++ pet/tasks.py
@@ -25,8 +25,8 @@ from typing import List, Dict, Callable
 import log
 from pet import task_helpers
 from pet.utils import InputExample
-from custom.winery_detection_processor import WineDataProcessor
-from custom.winery_detection_pvp import WineryTaskPVP
+from pet.winery_detection_processor import WineDataProcessor
+from pet.winery_detection_pvp import WineryTaskPVP
 
 logger = log.get_logger('root')
 
