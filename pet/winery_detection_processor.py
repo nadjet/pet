4369fa4a652e9e4b245b63ee0ba4150499881c0f
nadjet <nadjetba@gmail.com>
2021-11-28 11:15:10 +0100

    Replaced class name by correct one

--- pet/winery_detection_processor.py
+++ pet/winery_detection_processor.py
@@ -63,7 +63,7 @@ class WineryDataProcessor(DataProcessor):
         :param data_dir: the directory in which the training data can be found
         :return: a list of train examples
         """
-        return self._create_examples(os.path.join(data_dir, MyTaskDataProcessor.TRAIN_FILE_NAME), "train")
+        return self._create_examples(os.path.join(data_dir, WineryDataProcessor.TRAIN_FILE_NAME), "train")
 
     def get_dev_examples(self, data_dir: str) -> List[InputExample]:
         """
@@ -71,7 +71,7 @@ class WineryDataProcessor(DataProcessor):
         :param data_dir: the directory in which the dev data can be found
         :return: a list of dev examples
         """
-        return self._create_examples(os.path.join(data_dir, MyTaskDataProcessor.DEV_FILE_NAME), "dev")
+        return self._create_examples(os.path.join(data_dir, WineryDataProcessor.DEV_FILE_NAME), "dev")
 
     def get_test_examples(self, data_dir) -> List[InputExample]:
         """
@@ -79,7 +79,7 @@ class WineryDataProcessor(DataProcessor):
         :param data_dir: the directory in which the test data can be found
         :return: a list of test examples
         """
-        return self._create_examples(os.path.join(data_dir, MyTaskDataProcessor.TEST_FILE_NAME), "test")
+        return self._create_examples(os.path.join(data_dir, WineryDataProcessor.TEST_FILE_NAME), "test")
 
     def get_unlabeled_examples(self, data_dir) -> List[InputExample]:
         """
@@ -87,11 +87,11 @@ class WineryDataProcessor(DataProcessor):
         :param data_dir: the directory in which the unlabeled data can be found
         :return: a list of unlabeled examples
         """
-        return self._create_examples(os.path.join(data_dir, MyTaskDataProcessor.UNLABELED_FILE_NAME), "unlabeled")
+        return self._create_examples(os.path.join(data_dir, WineryDataProcessor.UNLABELED_FILE_NAME), "unlabeled")
 
     def get_labels(self) -> List[str]:
         """This method returns all possible labels for the task."""
-        return MyTaskDataProcessor.LABELS
+        return WineryDataProcessor.LABELS
 
     def _create_examples(self, path, set_type, max_examples=-1, skip_first=0):
         """Creates examples for the training and dev sets."""
@@ -101,9 +101,9 @@ class WineryDataProcessor(DataProcessor):
             reader = csv.reader(f, delimiter=',')
             for idx, row in enumerate(reader):
                 guid = "%s-%s" % (set_type, idx)
-                label = row[MyTaskDataProcessor.LABEL_COLUMN]
-                text_a = row[MyTaskDataProcessor.TEXT_A_COLUMN]
-                text_b = row[MyTaskDataProcessor.TEXT_B_COLUMN] if MyTaskDataProcessor.TEXT_B_COLUMN >= 0 else None
+                label = row[WineryDataProcessor.LABEL_COLUMN]
+                text_a = row[WineryDataProcessor.TEXT_A_COLUMN]
+                text_b = row[WineryDataProcessor.TEXT_B_COLUMN] if WineryDataProcessor.TEXT_B_COLUMN >= 0 else None
                 example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
                 examples.append(example)
 
@@ -114,4 +114,4 @@ class WineryDataProcessor(DataProcessor):
 PROCESSORS[WineryDataProcessor.TASK_NAME] = WineryDataProcessor
 
 # optional: if you have to use verbalizers that correspond to multiple tokens, uncomment the following line
-# TASK_HELPERS[MyTaskDataProcessor.TASK_NAME] = MultiMaskTaskHelper
+# TASK_HELPERS[WineryDataProcessor.TASK_NAME] = MultiMaskTaskHelper
