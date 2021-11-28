6e690ddc795dd56cb43b266134382295c31be367
nadjet <nadjetba@gmail.com>
2021-11-28 16:35:57 +0100

    Added processing of dev file and run to get new set of unlabelled data without dev data

--- data/process_unlabelled.py
+++ data/process_unlabelled.py
@@ -1,26 +1,30 @@
 import pandas as pd
 from nltk.tokenize import sent_tokenize
+from csv import writer
 
-train_df = pd.read_csv("train_winery.csv", header=None)
-test_df = pd.read_csv("test_winery.csv", header=None)
+train_df = pd.read_csv("train.csv", header=None)
+test_df = pd.read_csv("test.csv", header=None)
+dev_df = pd.read_csv("dev.csv", header=None)
 
 train_sentences = list(train_df[1].unique())
 test_sentences = list(test_df[1].unique())
+dev_sentences = list(dev_df[1].unique())
 
 print(len(train_sentences), len(test_sentences))
 
 sentences = []
 count_excluded = 0
-with open("unlabelled_winery.csv") as f:
+with open("unlabelled.csv") as f:
 	for line in f.readlines():
 		sentence = sent_tokenize(line)[0]
-		if sentence not in train_sentences and sentence not in test_sentences:
+		if sentence not in train_sentences and sentence not in test_sentences and sentence not in dev_sentences:
 			sentences.append(sentence)
 		else:
 			count_excluded+=1
 			print(f"Sentences excluded: {count_excluded}")
 
-with open("unlabelled_winery0.csv","w") as f:
+# create csv of unlabelled data with train, test or dev without data in tra
+with open("unlabelled0.csv","w") as f:
 	for sentence in sentences:
 		f.write(f"{sentence}\n")
 
