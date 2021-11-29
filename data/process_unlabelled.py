7a47380952fd9f33784f88463cecd85c5d604d8d
nadjet <nadjetba@gmail.com>
2021-11-29 10:42:41 +0100

    Fixed unlabeled dataset + random pick of 500

--- data/process_unlabelled.py
+++ data/process_unlabelled.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from nltk.tokenize import sent_tokenize
-from csv import writer
+import csv
 
+num = 600
 train_df = pd.read_csv("train.csv", header=None)
 test_df = pd.read_csv("test.csv", header=None)
 dev_df = pd.read_csv("dev.csv", header=None)
@@ -14,9 +15,10 @@ print(len(train_sentences), len(test_sentences))
 
 sentences = []
 count_excluded = 0
-with open("unlabeled.csv") as f:
-	for line in f.readlines():
-		sentence = sent_tokenize(line)[0]
+with open("unlabeled_original.csv","r") as f:
+	rd = csv.reader(f, delimiter=",")
+	for line in rd:
+		sentence = sent_tokenize(line[0])[0]
 		if sentence not in train_sentences and sentence not in test_sentences and sentence not in dev_sentences:
 			sentences.append(sentence)
 		else:
@@ -24,8 +26,12 @@ with open("unlabeled.csv") as f:
 			print(f"Sentences excluded: {count_excluded}")
 
 # create csv of unlabelled data with train, test or dev without data in tra
-with open("unlabeled0.csv","w") as f:
-	wr = writer(f, delimiter=",")
+
+import random
+sentences = list(set(random.choices(sentences, k=num)))
+print(len(sentences))
+with open("unlabeled.csv","w") as f:
+	wr = csv.writer(f, delimiter=",")
 	for sentence in sentences:
 		wr.writerow(["",sentence])
 
