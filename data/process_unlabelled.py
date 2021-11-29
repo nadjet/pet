9d216b96c06a6b7c52370f8c7e969a4395682f05
nadjet <nadjetba@gmail.com>
2021-11-29 11:33:49 +0100

    Fixed process_unlabelled to handle csv with rows with line breaks + proper split of sentences with stanza

--- data/process_unlabelled.py
+++ data/process_unlabelled.py
@@ -1,8 +1,13 @@
 import pandas as pd
-from nltk.tokenize import sent_tokenize
+import stanza
 import csv
 
-num = 600
+stanza.download('en')
+
+nlp = stanza.Pipeline(lang='en', processors='tokenize')
+
+
+num = 500
 train_df = pd.read_csv("train.csv", header=None)
 test_df = pd.read_csv("test.csv", header=None)
 dev_df = pd.read_csv("dev.csv", header=None)
@@ -13,23 +18,27 @@ dev_sentences = list(dev_df[1].unique())
 
 print(len(train_sentences), len(test_sentences))
 
+import re
 sentences = []
 count_excluded = 0
-with open("unlabeled_original.csv","r") as f:
-	rd = csv.reader(f, delimiter=",")
-	for line in rd:
-		sentence = sent_tokenize(line[0])[0]
+csv.register_dialect('mydialect', delimiter=',', quoting=csv.QUOTE_ALL, doublequote=True)
+df = pd.read_csv("unlabeled_original.csv", dialect='mydialect', header=None)
+
+df = df.sample(num)
+for i,sentence in enumerate(list(df[0])):
+	print(f"{i}/{len(list(df[0]))}")
+	doc = nlp(sentence)
+	for sentence in doc.sentences:
 		if sentence not in train_sentences and sentence not in test_sentences and sentence not in dev_sentences:
-			sentences.append(sentence)
+			sentences.append(sentence.text)
 		else:
 			count_excluded+=1
 			print(f"Sentences excluded: {count_excluded}")
+		break
 
 # create csv of unlabelled data with train, test or dev without data in tra
 
-import random
-sentences = list(set(random.choices(sentences, k=num)))
-print(len(sentences))
+
 with open("unlabeled.csv","w") as f:
 	wr = csv.writer(f, delimiter=",")
 	for sentence in sentences:
